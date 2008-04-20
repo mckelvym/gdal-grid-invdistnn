mckelvym/gdal-grid-invdistnn

commit 5550821d89842f967f00425e72b693b3b9ed4437

Allow 'POINT EMPTY' in WKT (bug #1628)
git-svn-id: https://svn.osgeo.org/gdal@13909 f0d54148-0727-0410-94bb-9a71ac55c965

trunk/gdal/ogr/ogrpolygon.cpp
@@ -1012,3 +1012,15 @@ void OGRPolygon::setCoordinateDimension( int nNewDimension )
     OGRGeometry::setCoordinateDimension( nNewDimension );
 }
 
+
+/************************************************************************/
+/*                               IsEmpty()                              */
+/************************************************************************/
+
+OGRBoolean OGRPolygon::IsEmpty(  ) const
+{
+    if (nRingCount == 0)
+        return TRUE;
+    else
+        return OGRPolygon::IsEmpty();
+}

trunk/gdal/ogr/ogr_geometry.h
@@ -188,8 +188,8 @@ class CPL_DLL OGRPoint : public OGRGeometry
 
     // Non standard
     virtual void setCoordinateDimension( int nDimension ); 
-    void        setX( double xIn ) { x = xIn; }
-    void        setY( double yIn ) { y = yIn; }
+    void        setX( double xIn ) { x = xIn; if (nCoordDimension == 0) nCoordDimension = 2; }
+    void        setY( double yIn ) { y = yIn; if (nCoordDimension == 0) nCoordDimension = 2; }
     void        setZ( double zIn ) { z = zIn; nCoordDimension=3; }
 
     // ISpatialRelation
@@ -201,6 +201,7 @@ class CPL_DLL OGRPoint : public OGRGeometry
     virtual OGRErr  transform( OGRCoordinateTransformation *poCT );
     virtual void flattenTo2D();
 
+    virtual OGRBoolean  IsEmpty() const;
 };
 
 /************************************************************************/
@@ -438,6 +439,8 @@ class CPL_DLL OGRPolygon : public OGRSurface
     OGRBoolean IsPointOnSurface( const OGRPoint * ) const;
 
     virtual void closeRings();
+
+    virtual OGRBoolean  IsEmpty() const;
 };
 
 /************************************************************************/

trunk/gdal/ogr/ogrpoint.cpp
@@ -44,9 +44,7 @@ CPL_CVSID("$Id$");
 OGRPoint::OGRPoint()
 
 {
-    x = 0;
-    y = 0;
-    z = 0;
+    empty();
 }
 
 /************************************************************************/
@@ -112,6 +110,7 @@ void OGRPoint::empty()
 
 {
     x = y = z = 0.0;
+    nCoordDimension = 0;
 }
 
 /************************************************************************/
@@ -155,7 +154,8 @@ void OGRPoint::flattenTo2D()
 
 {
     z = 0;
-    nCoordDimension = 2;
+    if (nCoordDimension > 2)
+        nCoordDimension = 2;
 }
 
 /************************************************************************/
@@ -407,9 +407,14 @@ OGRErr OGRPoint::exportToWkt( char ** ppszDstText ) const
     char        szTextEquiv[140];
     char        szCoordinate[80];
 
-    OGRMakeWktCoordinate(szCoordinate, x, y, z, nCoordDimension );
-    sprintf( szTextEquiv, "POINT (%s)", szCoordinate );
-    *ppszDstText = CPLStrdup( szTextEquiv );
+    if (nCoordDimension == 0)
+        *ppszDstText = CPLStrdup( "POINT EMPTY" );
+    else
+    {
+        OGRMakeWktCoordinate(szCoordinate, x, y, z, nCoordDimension );
+        sprintf( szTextEquiv, "POINT (%s)", szCoordinate );
+        *ppszDstText = CPLStrdup( szTextEquiv );
+    }
     
     return OGRERR_NONE;
 }
@@ -525,4 +530,13 @@ OGRErr OGRPoint::transform( OGRCoordinateTransformation *poCT )
     else
         return OGRERR_FAILURE;
 #endif
+}
+
+/************************************************************************/
+/*                               IsEmpty()                              */
+/************************************************************************/
+
+OGRBoolean OGRPoint::IsEmpty(  ) const
+{
+    return nCoordDimension == 0;
 }

commit 7fb7d6dc2162f0ebcd0ac145fd2b14594159fd23

Fix bug in OGRPolygon::IsEmpty() introduced by r13909
git-svn-id: https://svn.osgeo.org/gdal@14331 f0d54148-0727-0410-94bb-9a71ac55c965

trunk/gdal/ogr/ogrpolygon.cpp
@@ -1022,5 +1022,5 @@ OGRBoolean OGRPolygon::IsEmpty(  ) const
     if (nRingCount == 0)
         return TRUE;
     else
-        return OGRPolygon::IsEmpty();
+        return OGRGeometry::IsEmpty();
 }