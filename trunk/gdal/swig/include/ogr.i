mckelvym/gdal-grid-invdistnn

commit 2605eebd0b74f2a29537b44867957b84a7b3436a

Add support for GetFieldAsStringList, GetFieldAsDoubleList and GetFieldAsIntegerList in the C# wrapper (#2839)
git-svn-id: https://svn.osgeo.org/gdal@16310 f0d54148-0727-0410-94bb-9a71ac55c965

trunk/gdal/swig/include/ogr.i
@@ -745,34 +745,56 @@ public:
   }
   %clear (int *);
 
-#ifdef SWIGJAVA
+#if definied(SWIGJAVA)
   retIntArray GetFieldAsIntegerList(int id, int *nLen, const int **pList) {
       *pList = OGR_F_GetFieldAsIntegerList(self, id, nLen);
       return (retIntArray)*pList;
   }
+#else if definied(SWIGCSHARP)
+  %apply (int *intList) {const int *};
+  %apply (int *hasval) {int *count};
+  const int *GetFieldAsIntegerList(int id, int *count) {
+      return OGR_F_GetFieldAsIntegerList(self, id, count);
+  }
+  %clear (const int *);
+  %clear (int *count);
 #else
   void GetFieldAsIntegerList(int id, int *nLen, const int **pList) {
       *pList = OGR_F_GetFieldAsIntegerList(self, id, nLen);
   }
 #endif
 
-#ifdef SWIGJAVA
+#if definied(SWIGJAVA)
   retDoubleArray GetFieldAsDoubleList(int id, int *nLen, const double **pList) {
       *pList = OGR_F_GetFieldAsDoubleList(self, id, nLen);
       return (retDoubleArray)*pList;
   }
+#else if definied(SWIGCSHARP)
+  %apply (double *doubleList) {const double *};
+  %apply (int *hasval) {int *count};
+  const double *GetFieldAsDoubleList(int id, int *count) {
+      return OGR_F_GetFieldAsDoubleList(self, id, count);
+  }
+  %clear (const double *);
+  %clear (int *count);
 #else
   void GetFieldAsDoubleList(int id, int *nLen, const double **pList) {
       *pList = OGR_F_GetFieldAsDoubleList(self, id, nLen);
   }
 #endif
 
-#ifdef SWIGJAVA
+#if definied(SWIGJAVA)
   %apply (char** retAsStringArrayNoFree) {(char**)};
   char** GetFieldAsStringList(int id) {
       return OGR_F_GetFieldAsStringList(self, id);
   }
   %clear char**;
+#else if definied(SWIGCSHARP)
+  %apply (char **options) {char **};
+  char **GetFieldAsStringList(int id) {
+      return OGR_F_GetFieldAsStringList(self, id);
+  }
+  %clear (char **);
 #else
   void GetFieldAsStringList(int id, char ***pList) {
       *pList = OGR_F_GetFieldAsStringList(self, id);

trunk/gdal/swig/csharp/apps/ogrinfo.cs
@@ -171,7 +171,39 @@ class OGRInfo {
 				fdef.GetFieldTypeName(fdef.GetFieldType()) + ") = ");
 
 			if( feat.IsFieldSet( iField ) )
-				Console.WriteLine( feat.GetFieldAsString( iField ) );
+            {
+                if (fdef.GetFieldType() == FieldType.OFTStringList)
+                {
+                    string[] sList = feat.GetFieldAsStringList(iField);
+                    foreach (string s in sList)
+                    {
+                        Console.Write("\"" + s + "\" ");
+                    }
+                    Console.WriteLine();
+                }
+                else if (fdef.GetFieldType() == FieldType.OFTIntegerList)
+                {
+                    int count;
+                    int[] iList = feat.GetFieldAsIntegerList(iField, out count);
+                    for (int i = 0; i < count; i++)
+                    {
+                        Console.Write(iList[i] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                else if (fdef.GetFieldType() == FieldType.OFTRealList)
+                {
+                    int count;
+                    double[] iList = feat.GetFieldAsDoubleList(iField, out count);
+                    for (int i = 0; i < count; i++)
+                    {
+                        Console.Write(iList[i].ToString() + " ");
+                    }
+                    Console.WriteLine();
+                }
+                else
+                    Console.WriteLine(feat.GetFieldAsString(iField));
+            }
 			else
 				Console.WriteLine( "(null)" );
             

trunk/gdal/swig/include/csharp/typemaps_csharp.i
@@ -189,6 +189,36 @@ OPTIONAL_POD(int, int);
         return ret;
 }
 
+%typemap(imtype, out="IntPtr") int *intList "int[]"
+%typemap(cstype) int *intList %{int[]%}
+%typemap(in) int *intList %{ $1 = ($1_ltype)$input; %}
+%typemap(out) int *intList %{ $result = $1; %}
+%typemap(csout, excode=SWIGEXCODE) int *intList {
+        /* %typemap(csout) int *intList */
+        IntPtr cPtr = $imcall;
+        int[] ret = new int[count];
+        if (count > 0) {       
+	        System.Runtime.InteropServices.Marshal.Copy(cPtr, ret, 0, count);
+        }
+        $excode
+        return ret;
+}
+
+%typemap(imtype, out="IntPtr") double *doubleList "double[]"
+%typemap(cstype) double *doubleList %{double[]%}
+%typemap(in) double *doubleList %{ $1 = ($1_ltype)$input; %}
+%typemap(out) double *doubleList %{ $result = $1; %}
+%typemap(csout, excode=SWIGEXCODE) double *doubleList {
+        /* %typemap(csout) int *intList */
+        IntPtr cPtr = $imcall;
+        double[] ret = new double[count];
+        if (count > 0) {       
+	        System.Runtime.InteropServices.Marshal.Copy(cPtr, ret, 0, count);
+        }
+        $excode
+        return ret;
+}
+
 /*
  * Typemap for char **argout. 
  */

commit 2090db86dcc0eef1db45f253fb05609ce8b99ff4

Fix typo introduced in r16310
git-svn-id: https://svn.osgeo.org/gdal@16312 f0d54148-0727-0410-94bb-9a71ac55c965

trunk/gdal/swig/include/ogr.i
@@ -745,12 +745,12 @@ public:
   }
   %clear (int *);
 
-#if definied(SWIGJAVA)
+#if defined(SWIGJAVA)
   retIntArray GetFieldAsIntegerList(int id, int *nLen, const int **pList) {
       *pList = OGR_F_GetFieldAsIntegerList(self, id, nLen);
       return (retIntArray)*pList;
   }
-#else if definied(SWIGCSHARP)
+#elif defined(SWIGCSHARP)
   %apply (int *intList) {const int *};
   %apply (int *hasval) {int *count};
   const int *GetFieldAsIntegerList(int id, int *count) {
@@ -764,12 +764,12 @@ public:
   }
 #endif
 
-#if definied(SWIGJAVA)
+#if defined(SWIGJAVA)
   retDoubleArray GetFieldAsDoubleList(int id, int *nLen, const double **pList) {
       *pList = OGR_F_GetFieldAsDoubleList(self, id, nLen);
       return (retDoubleArray)*pList;
   }
-#else if definied(SWIGCSHARP)
+#elif defined(SWIGCSHARP)
   %apply (double *doubleList) {const double *};
   %apply (int *hasval) {int *count};
   const double *GetFieldAsDoubleList(int id, int *count) {
@@ -783,13 +783,13 @@ public:
   }
 #endif
 
-#if definied(SWIGJAVA)
+#if defined(SWIGJAVA)
   %apply (char** retAsStringArrayNoFree) {(char**)};
   char** GetFieldAsStringList(int id) {
       return OGR_F_GetFieldAsStringList(self, id);
   }
   %clear char**;
-#else if definied(SWIGCSHARP)
+#elif defined(SWIGCSHARP)
   %apply (char **options) {char **};
   char **GetFieldAsStringList(int id) {
       return OGR_F_GetFieldAsStringList(self, id);