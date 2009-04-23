1eb85b7c82ec601525b24dd147b7ef01605a01d8
warmerdam <warmerdam@f0d54148-0727-0410-94bb-9a71ac55c965>
2009-04-23 16:38:56 +0000

    added irvine interleaved read test
    git-svn-id: https://svn.osgeo.org/gdal@16824 f0d54148-0727-0410-94bb-9a71ac55c965

--- sandbox/warmerdam/pcidsk/tests/testpcidskfile.cpp
+++ sandbox/warmerdam/pcidsk/tests/testpcidskfile.cpp
@@ -9,6 +9,7 @@ class PCIDSKFileTest : public CppUnit::TestFixture
  
     CPPUNIT_TEST( testOpenEltoro );
     CPPUNIT_TEST( testReadImage );
+    CPPUNIT_TEST( testReadPixelInterleavedImage );
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -17,6 +18,7 @@ public:
     void tearDown();
     void testOpenEltoro();
     void testReadImage();
+    void testReadPixelInterleavedImage();
 };
 
 // Registers the fixture into the 'registry'
@@ -41,6 +43,7 @@ void PCIDSKFileTest::testOpenEltoro()
     CPPUNIT_ASSERT( eltoro->GetWidth() == 1024 );
     CPPUNIT_ASSERT( eltoro->GetHeight() == 1024 );
     CPPUNIT_ASSERT( eltoro->GetChannels() == 1 );
+    CPPUNIT_ASSERT( strcmp(eltoro->GetInterleaving(),"BAND") == 0 );
 
     delete eltoro;
 }
@@ -60,8 +63,34 @@ void PCIDSKFileTest::testReadImage()
     chan1->ReadBlock( 3, data_line );
 
     CPPUNIT_ASSERT( data_line[2] == 38 );
+    CPPUNIT_ASSERT( chan1->GetType() == PCIDSK::CHN_8U );
     
     delete eltoro;
 
+}
+
+void PCIDSKFileTest::testReadPixelInterleavedImage()
+{
+    PCIDSKFile *irvine;
+    PCIDSKChannel *channel;
+    uint8 data_line[512*4];
+
+    irvine = PCIDSK::Open( "irvine.pix", "r", NULL );
+
+    CPPUNIT_ASSERT( irvine != NULL );
+    CPPUNIT_ASSERT( strcmp(irvine->GetInterleaving(),"PIXEL") == 0 );
+
+    channel = irvine->GetChannel(2);
+    channel->ReadBlock( 511, data_line );
+
+    CPPUNIT_ASSERT( data_line[511] == 22 );
+    
+    channel = irvine->GetChannel(10);
+    channel->ReadBlock( 511, data_line );
+
+    CPPUNIT_ASSERT( ((short *) data_line)[511] == 304 );
+    CPPUNIT_ASSERT( channel->GetType() == PCIDSK::CHN_16S );
+    
+    delete irvine;
 }
 
