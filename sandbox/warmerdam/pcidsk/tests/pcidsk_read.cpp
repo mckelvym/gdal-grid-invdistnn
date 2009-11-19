0aabe0848dcdea7bbba649fabf03e23b78c643df
warmerdam <warmerdam@f0d54148-0727-0410-94bb-9a71ac55c965>
2009-11-19 19:14:30 +0000

    add copyright headers, add vector read test
    git-svn-id: https://svn.osgeo.org/gdal@18047 f0d54148-0727-0410-94bb-9a71ac55c965

--- sandbox/warmerdam/pcidsk/tests/pcidsk_read.cpp
+++ sandbox/warmerdam/pcidsk/tests/pcidsk_read.cpp
@@ -1,3 +1,30 @@
+/******************************************************************************
+ *
+ * Purpose:  Commandline utility for listing contents of a PCIDSK file.
+ * 
+ ******************************************************************************
+ * Copyright (c) 2009
+ * PCI Geomatics, 50 West Wilmot Street, Richmond Hill, Ont, Canada
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+
 #include "pcidsk.h"
 #include "pcidsk_vectorsegment.h"
 #include <cassert>
@@ -34,11 +61,12 @@ static void ReportVectorSegment( PCIDSK::PCIDSKSegment *segobj )
         printf( "  Attribute fields:\n" );
         for( i = 0; i < field_count; i++ )
         {
-            printf( "    %s (%s) %d/%s\n",
+            printf( "    %s (%s) %d/%s fmt:%s\n",
                     vecseg->GetFieldName(i).c_str(), 
                     vecseg->GetFieldDescription(i).c_str(), 
                     (int) vecseg->GetFieldType(i), 
-                    PCIDSK::ShapeFieldTypeName(vecseg->GetFieldType(i)).c_str() );
+                    PCIDSK::ShapeFieldTypeName(vecseg->GetFieldType(i)).c_str(),
+                    vecseg->GetFieldFormat(i).c_str() );
         }
         
         printf( "\n" );
