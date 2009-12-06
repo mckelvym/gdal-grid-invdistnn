mckelvym/gdal-grid-invdistnn

commit 3df3795d9962de6b6fd60299cacaec8970e0ee40

Fix minor memory leak. (#1540)
git-svn-id: https://svn.osgeo.org/gdal@18188 f0d54148-0727-0410-94bb-9a71ac55c965

trunk/gdal/ogr/ogrsf_frmts/gml/gmlreader.cpp
@@ -85,6 +85,9 @@ IGMLReader *CreateGMLReader()
     return new GMLReader();
 }
 
+int GMLReader::m_bXercesInitialized = FALSE;
+int GMLReader::m_nInstanceCount = 0;
+
 /************************************************************************/
 /*                             GMLReader()                              */
 /************************************************************************/
@@ -92,6 +95,7 @@ IGMLReader *CreateGMLReader()
 GMLReader::GMLReader()
 
 {
+    m_nInstanceCount++;
     m_nClassCount = 0;
     m_papoClass = NULL;
 
@@ -129,6 +133,13 @@ GMLReader::~GMLReader()
     CPLFree( m_pszFilename );
 
     CleanupParser();
+
+    --m_nInstanceCount;
+    if( m_nInstanceCount == 0 && m_bXercesInitialized )
+    {
+        XMLPlatformUtils::Terminate();
+        m_bXercesInitialized = FALSE;
+    }
 
 #ifdef HAVE_EXPAT
     if (fpGML)
@@ -176,9 +187,8 @@ int GMLReader::SetupParser()
 
 {
 #if HAVE_XERCES == 1
-    static int bXercesInitialized = FALSE;
 
-    if( !bXercesInitialized )
+    if( !m_bXercesInitialized )
     {
         try
         {
@@ -192,7 +202,7 @@ int GMLReader::SetupParser()
                       tr_strdup(toCatch.getMessage()) );
             return FALSE;
         }
-        bXercesInitialized = TRUE;
+        m_bXercesInitialized = TRUE;
     }
 
     // Cleanup any old parser.
@@ -200,6 +210,9 @@ int GMLReader::SetupParser()
         CleanupParser();
 
     // Create and initialize parser.
+    XMLCh* xmlUriValid = NULL;
+    XMLCh* xmlUriNS = NULL;
+
     try{
         m_poSAXReader = XMLReaderFactory::createXMLReader();
     
@@ -211,11 +224,12 @@ int GMLReader::SetupParser()
         m_poSAXReader->setEntityResolver( m_poGMLHandler );
         m_poSAXReader->setDTDHandler( m_poGMLHandler );
 
+        xmlUriValid = XMLString::transcode("http://xml.org/sax/features/validation");
+        xmlUriNS = XMLString::transcode("http://xml.org/sax/features/namespaces");
+
 #if (OGR_GML_VALIDATION)
-        m_poSAXReader->setFeature(
-            XMLString::transcode("http://xml.org/sax/features/validation"), true);
-        m_poSAXReader->setFeature(
-            XMLString::transcode("http://xml.org/sax/features/namespaces"), true);
+        m_poSAXReader->setFeature( xmlUriValid, true);
+        m_poSAXReader->setFeature( xmlUriNS, true);
 
         m_poSAXReader->setFeature( XMLUni::fgSAX2CoreNameSpaces, true );
         m_poSAXReader->setFeature( XMLUni::fgXercesSchema, true );
@@ -232,9 +246,14 @@ int GMLReader::SetupParser()
 #endif
 
 #endif
+        XMLString::release( &xmlUriValid );
+        XMLString::release( &xmlUriNS );
     }
     catch (...)
     {
+        XMLString::release( &xmlUriValid );
+        XMLString::release( &xmlUriNS );
+
         CPLError( CE_Warning, CPLE_AppDefined,
                   "Exception initializing Xerces based GML reader.\n" );
         return FALSE;

trunk/gdal/ogr/ogrsf_frmts/gml/gmlreaderp.h
@@ -200,6 +200,8 @@ public:
 class GMLReader : public IGMLReader 
 {
 private:
+    static int    m_bXercesInitialized;
+    static int    m_nInstanceCount;
     int           m_bClassListLocked;
 
     int         m_nClassCount;

commit 0573a5f819eb9dfeea97d253816560100743962e

Fix bug introduced by r18188. (#1540)
git-svn-id: https://svn.osgeo.org/gdal@18190 f0d54148-0727-0410-94bb-9a71ac55c965

trunk/gdal/ogr/ogrsf_frmts/gml/gmlreader.cpp
@@ -135,11 +135,13 @@ GMLReader::~GMLReader()
     CleanupParser();
 
     --m_nInstanceCount;
+#if HAVE_XERCES == 1
     if( m_nInstanceCount == 0 && m_bXercesInitialized )
     {
         XMLPlatformUtils::Terminate();
         m_bXercesInitialized = FALSE;
     }
+#endif
 
 #ifdef HAVE_EXPAT
     if (fpGML)