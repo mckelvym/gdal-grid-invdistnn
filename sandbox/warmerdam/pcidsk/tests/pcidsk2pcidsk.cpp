f855232f2aef950159d4eef1b51150098492d801
warmerdam <warmerdam@f0d54148-0727-0410-94bb-9a71ac55c965>
2009-12-12 03:14:30 +0000

    improve error handling and progress reporting
    git-svn-id: https://svn.osgeo.org/gdal@18271 f0d54148-0727-0410-94bb-9a71ac55c965

--- sandbox/warmerdam/pcidsk/tests/pcidsk2pcidsk.cpp
+++ sandbox/warmerdam/pcidsk/tests/pcidsk2pcidsk.cpp
@@ -78,118 +78,131 @@ int main( int argc, char **argv)
 /* -------------------------------------------------------------------- */
 /*      Open source file.                                               */
 /* -------------------------------------------------------------------- */
-    PCIDSKFile *src_file, *dst_file;
+    try
+    {
+        PCIDSKFile *src_file, *dst_file;
 
-    src_file = Open( src_filename, "r", NULL );
+        src_file = Open( src_filename, "r", NULL );
 
 /* -------------------------------------------------------------------- */
 /*      Create output file with similar options and channel types.      */
 /* -------------------------------------------------------------------- */
-    std::vector<eChanType> channel_types;
-    std::string options = src_file->GetInterleaving();
-    int i;
+        std::vector<eChanType> channel_types;
+        std::string options = src_file->GetInterleaving();
+        int i;
 
-    for( i = 0; i < src_file->GetChannels(); i++ )
-        channel_types.push_back( src_file->GetChannel(i+1)->GetType() );
+        for( i = 0; i < src_file->GetChannels(); i++ )
+            channel_types.push_back( src_file->GetChannel(i+1)->GetType() );
 
-    if( channel_types.size() > 0 && options == "FILE" ) // TILED?
-    {
-        PCIDSKChannel *src_chan = src_file->GetChannel(1);
-
-        if( src_chan->GetBlockHeight() > 1 )
+        if( channel_types.size() > 0 && options == "FILE" ) // TILED?
         {
-            char tile_size[128];
-            sprintf( tile_size, "%d", src_chan->GetBlockWidth() );
-            options = "TILED";
-            options += tile_size;
+            PCIDSKChannel *src_chan = src_file->GetChannel(1);
+
+            if( src_chan->GetBlockHeight() > 1 )
+            {
+                char tile_size[128];
+                sprintf( tile_size, "%d", src_chan->GetBlockWidth() );
+                options = "TILED";
+                options += tile_size;
+            }
         }
-    }
     
-    dst_file = Create( dst_filename, 
-                       src_file->GetWidth(), src_file->GetHeight(),
-                       channel_types.size(),
-                       &(channel_types[0]), options, NULL );
+        dst_file = Create( dst_filename, 
+                           src_file->GetWidth(), src_file->GetHeight(),
+                           channel_types.size(),
+                           &(channel_types[0]), options, NULL );
 
 /* -------------------------------------------------------------------- */
 /*      Copy georeferencing.                                            */
 /* -------------------------------------------------------------------- */
-    PCIDSKGeoref*src_geo = dynamic_cast<PCIDSKGeoref*>(src_file->GetSegment(1));
+        PCIDSKGeoref*src_geo = dynamic_cast<PCIDSKGeoref*>(src_file->GetSegment(1));
 
-    if( src_geo != NULL )
-    {
-        double a1, a2, xrot, b1, yrot, b3;
+        if( src_geo != NULL )
+        {
+            double a1, a2, xrot, b1, yrot, b3;
 
-        PCIDSKGeoref *dst_geo = 
-            dynamic_cast<PCIDSKGeoref*>(dst_file->GetSegment(1));
+            PCIDSKGeoref *dst_geo = 
+                dynamic_cast<PCIDSKGeoref*>(dst_file->GetSegment(1));
 
-        src_geo->GetTransform( a1, a2, xrot, b1, yrot, b3 );
+            src_geo->GetTransform( a1, a2, xrot, b1, yrot, b3 );
         
-        dst_geo->WriteSimple( src_geo->GetGeosys(), 
-                              a1, a2, xrot, b1, yrot, b3 );
-    }
+            dst_geo->WriteSimple( src_geo->GetGeosys(), 
+                                  a1, a2, xrot, b1, yrot, b3 );
+        }
     
 /* -------------------------------------------------------------------- */
 /*      Transfer over the contents band by band.                        */
 /* -------------------------------------------------------------------- */
-    int chan_num;
+        int chan_num;
 
-    for( chan_num = 1; chan_num <= src_file->GetChannels(); chan_num++ )
-    {
-        PCIDSKChannel *src_chan, *dst_chan;
-
-        src_chan = src_file->GetChannel(chan_num);
-        dst_chan = dst_file->GetChannel(chan_num);
-
-        if( src_chan->GetBlockWidth() != dst_chan->GetBlockWidth()
-            || src_chan->GetBlockHeight() != dst_chan->GetBlockHeight() )
-            ThrowPCIDSKException( "Output file block size does not match source." );
-        int block_size = src_chan->GetBlockWidth() * src_chan->GetBlockHeight()
-            * DataTypeSize( src_chan->GetType() );
-        int block_count = src_chan->GetBlockCount();
-
-        int blocks_in_buffer = 1;
-        if( buffersize >= 0 )
+        for( chan_num = 1; chan_num <= src_file->GetChannels(); chan_num++ )
         {
-            blocks_in_buffer = buffersize / block_size;
-            if( blocks_in_buffer == 0 )
-                blocks_in_buffer = 1;
-            if( blocks_in_buffer > block_count )
-                blocks_in_buffer = block_count;
-        }
+            PCIDSKChannel *src_chan, *dst_chan;
 
-        uint8 *image_block = (uint8 *) malloc(block_size * blocks_in_buffer );
+            src_chan = src_file->GetChannel(chan_num);
+            dst_chan = dst_file->GetChannel(chan_num);
+
+            if( src_chan->GetBlockWidth() != dst_chan->GetBlockWidth()
+                || src_chan->GetBlockHeight() != dst_chan->GetBlockHeight() )
+                ThrowPCIDSKException( "Output file block size does not match source." );
+            int block_size = src_chan->GetBlockWidth() * src_chan->GetBlockHeight()
+                * DataTypeSize( src_chan->GetType() );
+            int block_count = src_chan->GetBlockCount();
+
+            int blocks_in_buffer = 1;
+            if( buffersize >= 0 )
+            {
+                blocks_in_buffer = buffersize / block_size;
+                if( blocks_in_buffer == 0 )
+                    blocks_in_buffer = 1;
+                if( blocks_in_buffer > block_count )
+                    blocks_in_buffer = block_count;
+            }
+
+            uint8 *image_block = (uint8 *) malloc(block_size * blocks_in_buffer );
 
 /* -------------------------------------------------------------------- */
 /*      Process block by block.                                         */
 /* -------------------------------------------------------------------- */
-        int block;
+            int block;
 
-        for( block = 0; block < block_count;  )
-        {
-            int blocks_this_time = blocks_in_buffer;
-            int i;
+            for( block = 0; block < block_count;  )
+            {
+                int blocks_this_time = blocks_in_buffer;
+                int i;
 
-            if( blocks_this_time + block > block_count )
-                blocks_this_time = block_count - block;
+                if( blocks_this_time + block > block_count )
+                    blocks_this_time = block_count - block;
             
-            for( i = 0; i < blocks_this_time; i++ )
-                src_chan->ReadBlock( block+i, image_block + i * block_size );
+                for( i = 0; i < blocks_this_time; i++ )
+                    src_chan->ReadBlock( block+i, image_block + i * block_size );
             
-            for( i = 0; i < blocks_this_time; i++ )
-                dst_chan->WriteBlock( block+i, image_block + i * block_size );
+                for( i = 0; i < blocks_this_time; i++ )
+                    dst_chan->WriteBlock( block+i, image_block + i * block_size );
 
-            block += blocks_this_time;
+                block += blocks_this_time;
+            }
+
+            printf( "%d blocks transferred on channel %d, %d blocks at a time.\n", 
+                    block_count, chan_num, blocks_in_buffer );
+            fflush( stdout );
+
+            free( image_block );
         }
-
-        printf( "%d blocks transferred on channel %d, %d blocks at a time.\n", 
-                block_count, chan_num, blocks_in_buffer );
-
-        free( image_block );
-    }
 
 /* -------------------------------------------------------------------- */
 /*      Cleanup                                                         */
 /* -------------------------------------------------------------------- */
-    delete src_file;
-    delete dst_file;
+        delete src_file;
+        delete dst_file;
+    }
+/* ==================================================================== */
+/*      Catch any exception and report the message.                     */
+/* ==================================================================== */
+
+    catch( PCIDSK::PCIDSKException ex )
+    {
+        fprintf( stderr, "PCIDSKException:\n%s\n", ex.what() );
+        exit( 1 );
+    }
 }
