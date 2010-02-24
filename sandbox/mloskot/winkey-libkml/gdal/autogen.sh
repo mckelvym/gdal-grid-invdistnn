7c7cf2e912b389279353305cc2dc0b0863d9e5a8
mloskot <mloskot@f0d54148-0727-0410-94bb-9a71ac55c965>
2010-02-24 23:05:45 +0000

    sandbox/trunk: enabled liboolize in autogen.sh
    git-svn-id: https://svn.osgeo.org/gdal@18908 f0d54148-0727-0410-94bb-9a71ac55c965

--- sandbox/mloskot/winkey-libkml/gdal/autogen.sh
+++ sandbox/mloskot/winkey-libkml/gdal/autogen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -x
-#libtoolize --force --copy
+libtoolize --force --copy
 aclocal -I ./m4
 # We deliberately do not use autoheader, since it introduces PACKAGE junk
 # that conflicts with other packages in cpl_config.h.in (FrankW)
