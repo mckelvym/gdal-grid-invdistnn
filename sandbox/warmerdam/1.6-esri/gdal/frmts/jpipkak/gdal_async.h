24b4fd1b8d4cfa2ae14adccb0748d03f8a1dd582
warmerdam <warmerdam@f0d54148-0727-0410-94bb-9a71ac55c965>
2010-03-16 17:47:56 +0000

    preliminary backport of jpipkak driver and related machinery
    git-svn-id: https://svn.osgeo.org/gdal@19112 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ sandbox/warmerdam/1.6-esri/gdal/frmts/jpipkak/gdal_async.h
@@ -0,0 +1,65 @@
+/* ******************************************************************** */
+/*                          GDALAsyncReader                             */
+/* ******************************************************************** */
+
+/**
+* status of the asynchronous stream
+*/
+typedef enum 
+{	
+	GARIO_PENDING = 0,
+	GARIO_UPDATE = 1,
+	GARIO_ERROR = 2,
+	GARIO_COMPLETE = 3,
+	GARIO_TypeCount = 4
+} GDALAsyncStatusType;
+
+/**
+ * Class used as a session object for asynchronous requests.  They are
+ * created with GDALDataset::BeginAsyncReader(), and destroyed with
+ * GDALDataset::EndAsyncReader().
+ */
+class CPL_DLL GDALAsyncReader
+{
+  protected:
+    GDALDataset* poDS;
+    int          nXOff;
+    int          nYOff;
+    int          nXSize;
+    int          nYSize;
+    void *       pBuf;
+    int          nBufXSize;
+    int          nBufYSize;
+    GDALDataType eBufType;
+    int          nBandCount;
+    int*         panBandMap;
+    int          nPixelSpace;
+    int          nLineSpace;
+    int          nBandSpace;
+
+  public:
+    GDALAsyncReader() {}
+    virtual ~GDALAsyncReader() {}
+
+    GDALDataset* GetGDALDataset() {return poDS;}
+    int GetXOffset() {return nXOff;}
+    int GetYOffset() {return nYOff;}
+    int GetXSize() {return nXSize;}
+    int GetYSize() {return nYSize;}
+    void * GetBuffer() {return pBuf;}
+    int GetBufferXSize() {return nBufXSize;}
+    int GetBufferYSize() {return nBufYSize;}
+    GDALDataType GetBufferType() {return eBufType;}
+    int GetBandCount() {return nBandCount;}
+    int* GetBandMap() {return panBandMap;}
+    int GetPixelSpace() {return nPixelSpace;}
+    int GetLineSpace() {return nLineSpace;}
+    int GetBandSpace() {return nBandSpace;}
+
+    virtual GDALAsyncStatusType 
+        GetNextUpdatedRegion(double dfTimeout,
+                             int* pnBufXOff, int* pnBufYOff,
+                             int* pnBufXSize, int* pnBufXSize) = 0;
+    virtual int LockBuffer( double dfTimeout = -1.0 ) { return TRUE; }
+    virtual void UnlockBuffer() {}
+};
