mckelvym/gdal-grid-invdistnn

commit 446dcb4dd69eb270bcc104151e7ad438c20112fa

Use sibling file list to look for .aux.xml, .aux, .ovr, world files, tab files
git-svn-id: https://svn.osgeo.org/gdal@21992 f0d54148-0727-0410-94bb-9a71ac55c965

trunk/gdal/gcore/gdal_pam.h
@@ -117,10 +117,10 @@ class CPL_DLL GDALPamDataset : public GDALDataset
     virtual CPLXMLNode *SerializeToXML( const char *);
     virtual CPLErr      XMLInit( CPLXMLNode *, const char * );
     
-    virtual CPLErr TryLoadXML();
+    virtual CPLErr TryLoadXML(char **papszSiblingFiles = NULL);
     virtual CPLErr TrySaveXML();
 
-    CPLErr  TryLoadAux();
+    CPLErr  TryLoadAux(char **papszSiblingFiles = NULL);
     CPLErr  TrySaveAux();
 
     virtual const char *BuildPamFilename();

trunk/gdal/gcore/gdalpamdataset.cpp
@@ -635,7 +635,7 @@ const char *GDALPamDataset::BuildPamFilename()
 /*                             TryLoadXML()                             */
 /************************************************************************/
 
-CPLErr GDALPamDataset::TryLoadXML()
+CPLErr GDALPamDataset::TryLoadXML(char **papszSiblingFiles)
 
 {
     CPLXMLNode *psTree = NULL;
@@ -660,6 +660,19 @@ CPLErr GDALPamDataset::TryLoadXML()
 
     VSIStatBufL sStatBuf;
 
+    if (papszSiblingFiles != NULL)
+    {
+        int iSibling = CSLFindString( papszSiblingFiles,
+                                      CPLGetFilename(psPam->pszPamFilename) );
+        if( iSibling >= 0 )
+        {
+            CPLErrorReset();
+            CPLPushErrorHandler( CPLQuietErrorHandler );
+            psTree = CPLParseXMLFile( psPam->pszPamFilename );
+            CPLPopErrorHandler();
+        }
+    }
+    else
     if( VSIStatExL( psPam->pszPamFilename, &sStatBuf,
                     VSI_STAT_EXISTS_FLAG | VSI_STAT_NATURE_FLAG ) == 0
         && VSI_ISREG( sStatBuf.st_mode ) )
@@ -705,7 +718,7 @@ CPLErr GDALPamDataset::TryLoadXML()
 /*      If we fail, try .aux.                                           */
 /* -------------------------------------------------------------------- */
     if( psTree == NULL )
-        return TryLoadAux();
+        return TryLoadAux(papszSiblingFiles);
 
 /* -------------------------------------------------------------------- */
 /*      Initialize ourselves from this XML tree.                        */
@@ -1002,18 +1015,29 @@ char **GDALPamDataset::GetFileList()
     VSIStatBufL sStatBuf;
     char **papszFileList = GDALDataset::GetFileList();
 
-    if( psPam && psPam->osPhysicalFilename.size() > 0 
+    if( psPam && psPam->osPhysicalFilename.size() > 0
         && CSLFindString( papszFileList, psPam->osPhysicalFilename ) == -1 )
     {
         papszFileList = CSLInsertString( papszFileList, 0, 
                                          psPam->osPhysicalFilename );
     }
 
-    if( psPam && psPam->pszPamFilename 
-        && (nPamFlags & GPF_DIRTY 
-            || VSIStatExL( psPam->pszPamFilename, &sStatBuf, VSI_STAT_EXISTS_FLAG ) == 0) )
+    if( psPam && psPam->pszPamFilename )
     {
-        papszFileList = CSLAddString( papszFileList, psPam->pszPamFilename );
+        int bAddPamFile = (nPamFlags & GPF_DIRTY);
+        if (!bAddPamFile)
+        {
+            if (oOvManager.GetSiblingFiles() != NULL)
+                bAddPamFile = CSLFindString(oOvManager.GetSiblingFiles(),
+                                  CPLGetFilename(psPam->pszPamFilename)) >= 0;
+            else
+                bAddPamFile = VSIStatExL( psPam->pszPamFilename, &sStatBuf,
+                                          VSI_STAT_EXISTS_FLAG ) == 0;
+        }
+        if (bAddPamFile)
+        {
+            papszFileList = CSLAddString( papszFileList, psPam->pszPamFilename );
+        }
     }
 
     if( psPam && psPam->osAuxFilename.size() > 0 &&
@@ -1321,7 +1345,7 @@ char **GDALPamDataset::GetMetadata( const char *pszDomain )
 /*                             TryLoadAux()                             */
 /************************************************************************/
 
-CPLErr GDALPamDataset::TryLoadAux()
+CPLErr GDALPamDataset::TryLoadAux(char **papszSiblingFiles)
 
 {
 /* -------------------------------------------------------------------- */
@@ -1343,10 +1367,26 @@ CPLErr GDALPamDataset::TryLoadAux()
     if( strlen(pszPhysicalFile) == 0 )
         return CE_None;
 
+    if( papszSiblingFiles )
+    {
+        CPLString osAuxFilename = CPLResetExtension( pszPhysicalFile, "aux");
+        int iSibling = CSLFindString( papszSiblingFiles,
+                                      CPLGetFilename(osAuxFilename) );
+        if( iSibling < 0 )
+        {
+            osAuxFilename = pszPhysicalFile;
+            osAuxFilename += ".aux";
+            iSibling = CSLFindString( papszSiblingFiles,
+                                      CPLGetFilename(osAuxFilename) );
+            if( iSibling < 0 )
+                return CE_None;
+        }
+    }
+
 /* -------------------------------------------------------------------- */
 /*      Try to open .aux file.                                          */
 /* -------------------------------------------------------------------- */
-    GDALDataset *poAuxDS = GDALFindAssociatedAuxFile( pszPhysicalFile, 
+    GDALDataset *poAuxDS = GDALFindAssociatedAuxFile( pszPhysicalFile,
                                                       GA_ReadOnly, this );
 
     if( poAuxDS == NULL )

trunk/gdal/gcore/gdal_misc.cpp
@@ -1441,13 +1441,38 @@ int CPL_STDCALL GDALReadTabFile( const char * pszBaseFilename,
 
 
 {
+    return GDALReadTabFile2(pszBaseFilename, padfGeoTransform,
+                                       ppszWKT, pnGCPCount, ppasGCPs,
+                                       NULL);
+}
+
+
+int GDALReadTabFile2( const char * pszBaseFilename,
+                                 double *padfGeoTransform, char **ppszWKT,
+                                 int *pnGCPCount, GDAL_GCP **ppasGCPs,
+                                 char** papszSiblingFiles )
+{
     const char	*pszTAB;
     VSILFILE	*fpTAB;
+
+    pszTAB = CPLResetExtension( pszBaseFilename, "tab" );
+
+    if (papszSiblingFiles)
+    {
+        int iSibling = CSLFindString(papszSiblingFiles, CPLGetFilename(pszTAB));
+        if (iSibling >= 0)
+        {
+            CPLString osTabFilename = CPLFormFilename(CPLGetPath(pszBaseFilename),
+                                            papszSiblingFiles[iSibling], NULL);
+            return GDALLoadTabFile(osTabFilename, padfGeoTransform, ppszWKT,
+                                   pnGCPCount, ppasGCPs );
+        }
+        return FALSE;
+    }
 
 /* -------------------------------------------------------------------- */
 /*      Try lower case, then upper case.                                */
 /* -------------------------------------------------------------------- */
-    pszTAB = CPLResetExtension( pszBaseFilename, "tab" );
 
     fpTAB = VSIFOpenL( pszTAB, "rt" );
 
@@ -1597,6 +1622,13 @@ GDALReadWorldFile( const char *pszBaseFilename, const char *pszExtension,
                    double *padfGeoTransform )
 
 {
+    return GDALReadWorldFile2(pszBaseFilename, pszExtension,
+                                         padfGeoTransform, NULL);
+}
+
+int GDALReadWorldFile2( const char *pszBaseFilename, const char *pszExtension,
+                                   double *padfGeoTransform, char** papszSiblingFiles )
+{
     const char  *pszTFW;
     char        szExtUpper[32], szExtLower[32];
     int         i;
@@ -1622,8 +1654,8 @@ GDALReadWorldFile( const char *pszBaseFilename, const char *pszExtension,
         szDerivedExtension[2] = 'w';
         szDerivedExtension[3] = '\0';
         
-        if( GDALReadWorldFile( pszBaseFilename, szDerivedExtension, 
-                               padfGeoTransform ) )
+        if( GDALReadWorldFile2( pszBaseFilename, szDerivedExtension,
+                               padfGeoTransform, papszSiblingFiles ) )
             return TRUE;
 
         // unix version - extension + 'w'
@@ -1632,8 +1664,8 @@ GDALReadWorldFile( const char *pszBaseFilename, const char *pszExtension,
 
         strcpy( szDerivedExtension, oBaseExt.c_str() );
         strcat( szDerivedExtension, "w" );
-        return GDALReadWorldFile( pszBaseFilename, szDerivedExtension, 
-                                  padfGeoTransform );
+        return GDALReadWorldFile2( pszBaseFilename, szDerivedExtension,
+                                  padfGeoTransform, papszSiblingFiles );
     }
 
 /* -------------------------------------------------------------------- */
@@ -1654,13 +1686,27 @@ GDALReadWorldFile( const char *pszBaseFilename, const char *pszExtension,
         szExtLower[i] = (char) tolower(szExtLower[i]);
     }
 
-/* -------------------------------------------------------------------- */
-/*      Try lower case, then upper case.                                */
-/* -------------------------------------------------------------------- */
     VSIStatBufL sStatBuf;
     int bGotTFW;
 
     pszTFW = CPLResetExtension( pszBaseFilename, szExtLower );
+
+    if (papszSiblingFiles)
+    {
+        int iSibling = CSLFindString(papszSiblingFiles, CPLGetFilename(pszTFW));
+        if (iSibling >= 0)
+        {
+            CPLString osTFWFilename = CPLFormFilename(CPLGetPath(pszBaseFilename),
+                                                papszSiblingFiles[iSibling], NULL);
+            return GDALLoadWorldFile( osTFWFilename, padfGeoTransform );
+        }
+        else
+            return FALSE;
+    }
+
+/* -------------------------------------------------------------------- */
+/*      Try lower case, then upper case.                                */
+/* -------------------------------------------------------------------- */
 
     bGotTFW = VSIStatExL( pszTFW, &sStatBuf, VSI_STAT_EXISTS_FLAG ) == 0;
 

trunk/gdal/gcore/gdal_priv.h
@@ -203,6 +203,35 @@ class CPL_DLL GDALDefaultOverviews
     int        HaveMaskFile( char **papszSiblings = NULL, 
                              const char *pszBasename = NULL );
 
+    char**     GetSiblingFiles() { return papszInitSiblingFiles; }
+};
+
+/* ******************************************************************** */
+/*                             GDALOpenInfo                             */
+/*                                                                      */
+/*      Structure of data about dataset for open functions.             */
+/* ******************************************************************** */
+
+class CPL_DLL GDALOpenInfo
+{
+  public:
+                GDALOpenInfo( const char * pszFile, GDALAccess eAccessIn,
+                              char **papszSiblingFiles = NULL );
+                ~GDALOpenInfo( void );
+
+    char        *pszFilename;
+    char        **papszSiblingFiles;
+
+    GDALAccess  eAccess;
+
+    int         bStatOK;
+    int         bIsDirectory;
+
+    FILE        *fp;
+
+    int         nHeaderBytes;
+    GByte       *pabyHeader;
+
 };
 
 /* ******************************************************************** */
@@ -211,6 +240,8 @@ class CPL_DLL GDALDefaultOverviews
 
 /* Internal method for now. Might be subject to later revisions */
 GDALDatasetH GDALOpenInternal( const char * pszFilename, GDALAccess eAccess,
+                               const char* const * papszAllowedDrivers);
+GDALDatasetH GDALOpenInternal( GDALOpenInfo& oOpenInfo,
                                const char* const * papszAllowedDrivers);
 
 //! A set of associated raster bands, usually from one file.
@@ -222,6 +253,8 @@ class CPL_DLL GDALDataset : public GDALMajorObject
 
     /* Internal method for now. Might be subject to later revisions */
     friend GDALDatasetH GDALOpenInternal( const char *, GDALAccess, const char* const * papszAllowedDrivers);
+    friend GDALDatasetH GDALOpenInternal( GDALOpenInfo& oOpenInfo,
+                                          const char* const * papszAllowedDrivers);
 
     friend class GDALDriver;
     friend class GDALDefaultOverviews;
@@ -621,34 +654,6 @@ class CPL_DLL GDALNoDataValuesMaskBand : public GDALRasterBand
 };
 
 /* ******************************************************************** */
-/*                             GDALOpenInfo                             */
-/*                                                                      */
-/*      Structure of data about dataset for open functions.             */
-/* ******************************************************************** */
-
-class CPL_DLL GDALOpenInfo
-{
-  public:
-                GDALOpenInfo( const char * pszFile, GDALAccess eAccessIn,
-                              char **papszSiblingFiles = NULL );
-                ~GDALOpenInfo( void );
-    
-    char        *pszFilename;
-    char        **papszSiblingFiles;
-
-    GDALAccess  eAccess;
-
-    int         bStatOK;
-    int         bIsDirectory;
-
-    FILE        *fp;
-
-    int         nHeaderBytes;
-    GByte       *pabyHeader;
-
-};
-
-/* ******************************************************************** */
 /*                              GDALDriver                              */
 /* ******************************************************************** */
 
@@ -906,6 +911,13 @@ int CPL_DLL GDALCheckBandCount( int nBands, int bIsZeroAllowed );
 #define EQUAL_TO_NODATA(dfValue, dfNoDataValue) \
  (dfValue == dfNoDataValue || (dfNoDataValue != 0 && fabs(1 - dfValue / dfNoDataValue) < 1e-10 ))
 
+/* Internal use only */
+int GDALReadWorldFile2( const char *pszBaseFilename, const char *pszExtension,
+                                   double *padfGeoTransform, char** papszSiblingFiles );
+int GDALReadTabFile2( const char * pszBaseFilename,
+                                 double *padfGeoTransform, char **ppszWKT,
+                                 int *pnGCPCount, GDAL_GCP **ppasGCPs,
+                                 char** papszSiblingFiles );
 
 CPL_C_END
 

trunk/gdal/gcore/gdaldataset.cpp
@@ -2034,8 +2034,14 @@ char **GDALDataset::GetFileList()
             szDerivedExtension[2] = 'w';
             szDerivedExtension[3] = '\0';
             CPLString osWorldFilename = CPLResetExtension( osMainFilename, szDerivedExtension );
-            
-            if( VSIStatExL( osWorldFilename, &sStat, VSI_STAT_EXISTS_FLAG ) == 0 )
+
+            if (oOvManager.papszInitSiblingFiles)
+            {
+                if (CSLFindString(oOvManager.papszInitSiblingFiles,
+                                  CPLGetFilename(osWorldFilename)) >= 0)
+                    papszList = CSLAddString( papszList, osWorldFilename );
+            }
+            else if( VSIStatExL( osWorldFilename, &sStat, VSI_STAT_EXISTS_FLAG ) == 0 )
                 papszList = CSLAddString( papszList, osWorldFilename );
         }
     }
@@ -2186,11 +2192,18 @@ GDALOpen( const char * pszFilename, GDALAccess eAccess )
 GDALDatasetH GDALOpenInternal( const char * pszFilename, GDALAccess eAccess,
                                const char* const * papszAllowedDrivers)
 {
-    VALIDATE_POINTER1( pszFilename, "GDALOpen", NULL );
+    GDALOpenInfo oOpenInfo( pszFilename, eAccess );
+    return GDALOpenInternal(oOpenInfo, papszAllowedDrivers);
+}
+
+GDALDatasetH GDALOpenInternal( GDALOpenInfo& oOpenInfo,
+                               const char* const * papszAllowedDrivers)
+{
+
+    VALIDATE_POINTER1( oOpenInfo.pszFilename, "GDALOpen", NULL );
 
     int         iDriver;
     GDALDriverManager *poDM = GetGDALDriverManager();
-    GDALOpenInfo oOpenInfo( pszFilename, eAccess );
     CPLLocaleC  oLocaleForcer;
 
     CPLErrorReset();
@@ -2220,11 +2233,11 @@ GDALDatasetH GDALOpenInternal( const char * pszFilename, GDALAccess eAccess,
 
             if( CPLGetPID() != GDALGetResponsiblePIDForCurrentThread() )
                 CPLDebug( "GDAL", "GDALOpen(%s, this=%p) succeeds as %s (pid=%d, responsiblePID=%d).",
-                          pszFilename, poDS, poDriver->GetDescription(),
+                          oOpenInfo.pszFilename, poDS, poDriver->GetDescription(),
                           (int)CPLGetPID(), (int)GDALGetResponsiblePIDForCurrentThread() );
             else
                 CPLDebug( "GDAL", "GDALOpen(%s, this=%p) succeeds as %s.",
-                          pszFilename, poDS, poDriver->GetDescription() );
+                          oOpenInfo.pszFilename, poDS, poDriver->GetDescription() );
 
             return (GDALDatasetH) poDS;
         }
@@ -2236,12 +2249,12 @@ GDALDatasetH GDALOpenInternal( const char * pszFilename, GDALAccess eAccess,
     if( oOpenInfo.bStatOK )
         CPLError( CE_Failure, CPLE_OpenFailed,
                   "`%s' not recognised as a supported file format.\n",
-                  pszFilename );
+                  oOpenInfo.pszFilename );
     else
         CPLError( CE_Failure, CPLE_OpenFailed,
                   "`%s' does not exist in the file system,\n"
                   "and is not recognised as a supported dataset name.\n",
-                  pszFilename );
+                  oOpenInfo.pszFilename );
 
     return NULL;
 }

trunk/gdal/gcore/gdaldefaultoverviews.cpp
@@ -203,7 +203,9 @@ void GDALDefaultOverviews::OverviewScan()
 
         if( bExists )
         {
-            poODS = (GDALDataset*) GDALOpen( osOvrFilename, poDS->GetAccess() );
+            GDALOpenInfo oOpenInfo(osOvrFilename, poDS->GetAccess(),
+                                   papszInitSiblingFiles);
+            poODS = (GDALDataset*) GDALOpenInternal( oOpenInfo, NULL );
         }
     }
 
@@ -217,8 +219,28 @@ void GDALDefaultOverviews::OverviewScan()
 /* -------------------------------------------------------------------- */
     if( !poODS && !EQUAL(pszInitName,":::VIRTUAL:::") )
     {
-        poODS = GDALFindAssociatedAuxFile( pszInitName, poDS->GetAccess(),
-                                           poDS );
+        int bTryFindAssociatedAuxFile = TRUE;
+        if( papszInitSiblingFiles )
+        {
+            CPLString osAuxFilename = CPLResetExtension( pszInitName, "aux");
+            int iSibling = CSLFindString( papszInitSiblingFiles,
+                                        CPLGetFilename(osAuxFilename) );
+            if( iSibling < 0 )
+            {
+                osAuxFilename = pszInitName;
+                osAuxFilename += ".aux";
+                iSibling = CSLFindString( papszInitSiblingFiles,
+                                        CPLGetFilename(osAuxFilename) );
+                if( iSibling < 0 )
+                    bTryFindAssociatedAuxFile = FALSE;
+            }
+        }
+
+        if (bTryFindAssociatedAuxFile)
+        {
+            poODS = GDALFindAssociatedAuxFile( pszInitName, poDS->GetAccess(),
+                                            poDS );
+        }
 
         if( poODS )
         {
@@ -992,7 +1014,9 @@ int GDALDefaultOverviews::HaveMaskFile( char ** papszSiblingFiles,
 /* -------------------------------------------------------------------- */
 /*      Open the file.                                                  */
 /* -------------------------------------------------------------------- */
-    poMaskDS = (GDALDataset *) GDALOpen( osMskFilename, poDS->GetAccess() );
+    GDALOpenInfo oOpenInfo(osMskFilename, poDS->GetAccess(),
+                           papszInitSiblingFiles);
+    poMaskDS = (GDALDataset *) GDALOpenInternal( oOpenInfo, NULL );
     CPLAssert( poMaskDS != poDS );
 
     if( poMaskDS == NULL )

commit 0aa5113a38def0bce8f7abd3b123b9a9d1084ee4

Fix PAM ProxyDB mechanism that was broken in r21992 (trunk-only); Add regression tests for this mechanism
git-svn-id: https://svn.osgeo.org/gdal@22920 f0d54148-0727-0410-94bb-9a71ac55c965

trunk/autotest/gcore/pam.py
@@ -31,6 +31,7 @@
 import os
 import sys
 import shutil
+import stat
 
 sys.path.append( '../pymod' )
 
@@ -350,6 +351,54 @@ def pam_10():
     return 'success'
 
 ###############################################################################
+# Test PamProxyDb mechanism
+
+def pam_11():
+
+    # Create a read-only directory
+    try:
+        os.chmod('tmpdirreadonly', stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+        shutil.rmtree('tmpdirreadonly')
+    except:
+        pass
+    os.mkdir('tmpdirreadonly')
+    shutil.copy('data/byte.tif', 'tmpdirreadonly/byte.tif')
+    os.chmod('tmpdirreadonly', stat.S_IRUSR | stat.S_IXUSR)
+
+    # Compute statistics --> the saving as .aux.xml should fail
+    ds = gdal.Open('tmpdirreadonly/byte.tif')
+    stats = ds.GetRasterBand(1).ComputeStatistics(False)
+    if stats[0] != 74:
+        gdaltest.post_reason('did not get expected minimum')
+        return 'fail'
+    gdal.ErrorReset()
+    ds = None
+    error_msg = gdal.GetLastErrorMsg()
+    if error_msg.find('Unable to save auxilary information') != 0:
+        gdaltest.post_reason('warning was expected at that point')
+        return 'fail'
+
+    # Check that we actually have no saved statistics
+    ds = gdal.Open('tmpdirreadonly/byte.tif')
+    stats = ds.GetRasterBand(1).GetStatistics(False, False)
+    if stats[3] != -1:
+        gdaltest.post_reason('did not expected to have stats at that point')
+        return 'fail'
+    ds = None
+
+    # This must be run as an external process so we can override GDAL_PAM_PROXY_DIR
+    # at the beginning of the process
+    import test_py_scripts
+    ret = test_py_scripts.run_py_script_as_external_script('.', 'pamproxydb', '-run')
+    #print(ret)
+    if ret.find('success') == -1:
+        gdaltest.post_reason('pamproxydb.py failed')
+        print(ret)
+        return 'fail'
+
+    return 'success'
+
+###############################################################################
 # Cleanup.
 
 def pam_cleanup():
@@ -358,6 +407,17 @@ def pam_cleanup():
         gdal.SetConfigOption( 'GDAL_PAM_ENABLED', gdaltest.pam_setting )
     else:
         gdal.SetConfigOption( 'GDAL_PAM_ENABLED', None )
+
+    try:
+        os.chmod('tmpdirreadonly', stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+        shutil.rmtree('tmpdirreadonly')
+    except:
+        pass
+    try:
+        shutil.rmtree('tmppamproxydir')
+    except:
+        pass
+
     return 'success'
 
 gdaltest_list = [
@@ -371,6 +431,7 @@ gdaltest_list = [
     pam_8,
     pam_9,
     pam_10,
+    pam_11,
     pam_cleanup ]
 
 if __name__ == '__main__':

trunk/autotest/gcore/pamproxydb.py
@@ -1,0 +1,117 @@
+#!/usr/bin/env python
+###############################################################################
+# $Id$
+#
+# Project:  GDAL/OGR Test Suite
+# Purpose:  Test functioning of the ProxyDB PAM metadata support
+# Author:   Even Rouault, <even dot rouault at mines dash paris dot org>
+#
+###############################################################################
+# Copyright (c) 2011, Even Rouault, <even dot rouault at mines dash paris dot org>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+###############################################################################
+
+import sys
+
+# Must to be launched from pam.py/pam_11()
+if len(sys.argv) == 2 and sys.argv[1] == '-run':
+
+    from osgeo import gdal
+    import os
+    import shutil
+
+    try:
+        shutil.rmtree('tmppamproxydir')
+    except:
+        pass
+    os.mkdir('tmppamproxydir')
+
+    gdal.SetConfigOption('GDAL_PAM_PROXY_DIR', 'tmppamproxydir')
+
+    # Compute statistics. They should be saved in the  .aux.xml in the proxyDB
+    ds = gdal.Open('tmpdirreadonly/byte.tif')
+    stats = ds.GetRasterBand(1).ComputeStatistics(False)
+    gdal.ErrorReset()
+    ds = None
+    error_msg = gdal.GetLastErrorMsg()
+    if error_msg != '':
+        print('did not expected error message')
+        sys.exit(1)
+
+    # Check that the .aux.xml in the proxyDB exists
+    filelist = gdal.ReadDir('tmppamproxydir')
+    if not '000000_tmpdirreadonly_byte.tif.aux.xml' in filelist:
+        print('did not get find 000000_tmpdirreadonly_byte.tif.aux.xml on filesystem')
+        sys.exit(1)
+
+    # Test altering a value to check that the file will be used
+    f = open('tmppamproxydir/000000_tmpdirreadonly_byte.tif.aux.xml', 'w')
+    f.write("""<PAMDataset>
+  <PAMRasterBand band="1">
+    <Metadata>
+      <MDI key="STATISTICS_MAXIMUM">255</MDI>
+      <MDI key="STATISTICS_MEAN">126.765</MDI>
+      <MDI key="STATISTICS_MINIMUM">-9999</MDI>
+      <MDI key="STATISTICS_STDDEV">22.928470838676</MDI>
+    </Metadata>
+  </PAMRasterBand>
+</PAMDataset>""")
+    f.close()
+
+    ds = gdal.Open('tmpdirreadonly/byte.tif')
+    filelist = ds.GetFileList()
+    if len(filelist) != 2:
+        print('did not get find 000000_tmpdirreadonly_byte.tif.aux.xml in dataset GetFileList()')
+        print(filelist)
+        sys.exit(1)
+
+    stats = ds.GetRasterBand(1).GetStatistics(False, False)
+    if stats[0] != -9999:
+        print('did not get expected minimum')
+        sys.exit(1)
+    ds = None
+
+    # Check that proxy overviews work
+    ds = gdal.Open('tmpdirreadonly/byte.tif')
+    ds.BuildOverviews('NEAR', overviewlist = [2])
+    ds = None
+
+    filelist = gdal.ReadDir('tmppamproxydir')
+    if not '000001_tmpdirreadonly_byte.tif.ovr' in filelist:
+        print('did not get find 000001_tmpdirreadonly_byte.tif.ovr')
+        sys.exit(1)
+
+    ds = gdal.Open('tmpdirreadonly/byte.tif')
+    filelist = ds.GetFileList()
+    if len(filelist) != 3:
+        print('did not get find 000001_tmpdirreadonly_byte.tif.ovr in dataset GetFileList()')
+        print(filelist)
+        sys.exit(1)
+    nb_ovr = ds.GetRasterBand(1).GetOverviewCount()
+    ds = None
+
+    if nb_ovr != 1:
+        print('did not get expected overview count')
+        sys.exit(1)
+
+    import testnonboundtoswig
+    testnonboundtoswig.GDALDestroyDriverManager()
+
+    print('success')

trunk/gdal/gcore/gdal_pam.h
@@ -109,6 +109,9 @@ class CPL_DLL GDALPamDataset : public GDALDataset
 {
     friend class GDALPamRasterBand;
 
+  private:
+    int IsPamFilenameAPotentialSiblingFile();
+
   protected:
                 GDALPamDataset(void);
 

trunk/gdal/gcore/gdalpamdataset.cpp
@@ -654,6 +654,33 @@ const char *GDALPamDataset::BuildPamFilename()
 }
 
 /************************************************************************/
+/*                   IsPamFilenameAPotentialSiblingFile()               */
+/************************************************************************/
+
+int GDALPamDataset::IsPamFilenameAPotentialSiblingFile()
+{
+    if (psPam == NULL)
+        return FALSE;
+
+/* -------------------------------------------------------------------- */
+/*      Determine if the PAM filename is a .aux.xml file next to the    */
+/*      physical file, or if it comes from the ProxyDB                  */
+/* -------------------------------------------------------------------- */
+    const char *pszPhysicalFile = psPam->osPhysicalFilename;
+
+    if( strlen(pszPhysicalFile) == 0 && GetDescription() != NULL )
+        pszPhysicalFile = GetDescription();
+
+    int nLenPhysicalFile = strlen(pszPhysicalFile);
+    int bIsSiblingPamFile = strncmp(psPam->pszPamFilename, pszPhysicalFile,
+                                    nLenPhysicalFile) == 0 &&
+                            strcmp(psPam->pszPamFilename + nLenPhysicalFile,
+                                   ".aux.xml") == 0;
+
+    return bIsSiblingPamFile;
+}
+
+/************************************************************************/
 /*                             TryLoadXML()                             */
 /************************************************************************/
 
@@ -682,7 +709,12 @@ CPLErr GDALPamDataset::TryLoadXML(char **papszSiblingFiles)
 
     VSIStatBufL sStatBuf;
 
-    if (papszSiblingFiles != NULL)
+/* -------------------------------------------------------------------- */
+/*      In case the PAM filename is a .aux.xml file next to the         */
+/*      physical file and we have a siblings list, then we can skip     */
+/*      stat'ing the filesystem.                                        */
+/* -------------------------------------------------------------------- */
+    if (papszSiblingFiles != NULL && IsPamFilenameAPotentialSiblingFile())
     {
         int iSibling = CSLFindString( papszSiblingFiles,
                                       CPLGetFilename(psPam->pszPamFilename) );
@@ -1049,7 +1081,7 @@ char **GDALPamDataset::GetFileList()
         int bAddPamFile = (nPamFlags & GPF_DIRTY);
         if (!bAddPamFile)
         {
-            if (oOvManager.GetSiblingFiles() != NULL)
+            if (oOvManager.GetSiblingFiles() != NULL && IsPamFilenameAPotentialSiblingFile())
                 bAddPamFile = CSLFindString(oOvManager.GetSiblingFiles(),
                                   CPLGetFilename(psPam->pszPamFilename)) >= 0;
             else