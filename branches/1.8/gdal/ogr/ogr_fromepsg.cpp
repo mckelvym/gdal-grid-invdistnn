mckelvym/gdal-grid-invdistnn

commit db9e9279ee93539f89734627f47af8be00349d54

ensure that the result of importFromEPSGA() always has keyword ordering fixed up (#4178)
git-svn-id: https://svn.osgeo.org/gdal@22820 f0d54148-0727-0410-94bb-9a71ac55c965

branches/1.8/gdal/ogr/ogr_fromepsg.cpp
@@ -2014,8 +2014,6 @@ OGRErr OGRSpatialReference::importFromEPSGA( int nCode )
             SetAuthority( "PROJCS", "EPSG", nCode );
         else if( IsGeographic() )
             SetAuthority( "GEOGCS", "EPSG", nCode );
-
-        eErr = FixupOrdering();
     }
 
 /* -------------------------------------------------------------------- */
@@ -2027,6 +2025,13 @@ OGRErr OGRSpatialReference::importFromEPSGA( int nCode )
                   "EPSG PCS/GCS code %d not found in EPSG support files.  Is this a valid\nEPSG coordinate system?", 
                   nCode );
     }
+
+/* -------------------------------------------------------------------- */
+/*      To the extent possible, we want to return the results in as     */
+/*      close to standard OGC format as possible, so we fixup the       */
+/*      ordering.                                                       */
+/* -------------------------------------------------------------------- */
+    eErr = FixupOrdering();
 
     return eErr;
 }

commit 20d762ef42b7a770d4fe099e5081bbb087a6d82d

fix importFromEPSG when target code is not found (bug #4345) - minor regression introduced in r22820 (bug #4178)
git-svn-id: https://svn.osgeo.org/gdal@23422 f0d54148-0727-0410-94bb-9a71ac55c965

branches/1.8/gdal/ogr/ogr_fromepsg.cpp
@@ -2031,7 +2031,10 @@ OGRErr OGRSpatialReference::importFromEPSGA( int nCode )
 /*      close to standard OGC format as possible, so we fixup the       */
 /*      ordering.                                                       */
 /* -------------------------------------------------------------------- */
-    eErr = FixupOrdering();
+    if( eErr == OGRERR_NONE )
+    {
+        eErr = FixupOrdering();
+    }
 
     return eErr;
 }