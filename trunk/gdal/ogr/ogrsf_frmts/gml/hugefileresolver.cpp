mckelvym/gdal-grid-invdistnn

commit 8c21943900356880023c131bae83ebe36a72e59b

hugefileresolver: use directly CPLXML API (#4380)
git-svn-id: https://svn.osgeo.org/gdal@23552 f0d54148-0727-0410-94bb-9a71ac55c965

trunk/gdal/ogr/ogrsf_frmts/gml/hugefileresolver.cpp
@@ -1750,14 +1750,13 @@ static int gmlHugeResolveEdges( struct huge_helper *helper,
                 else
                 {
                     /* rewriting a Child Node */
-                    CPLString gmlText = "<directedEdge";
+                    CPLXMLNode *psNewNode = CPLCreateXMLNode(NULL, CXT_Element, "directedEdge");
                     if( pChild->pItem->cOrientation == '-' )
-                        gmlText += " orientation=\"-\">";
-                    else
-                        gmlText += ">";
-                    gmlText += pChild->pItem->gmlText->c_str();
-                    gmlText += "</directedEdge>";
-                    CPLXMLNode *psNewNode = CPLParseXMLString( gmlText.c_str());
+                    {
+                        CPLXMLNode *psOrientationNode = CPLCreateXMLNode(psNewNode, CXT_Attribute, "orientation");
+                        CPLCreateXMLNode(psOrientationNode, CXT_Text, "-");
+                    }
+                    CPLCreateXMLNode(psNewNode, CXT_Text, pChild->pItem->gmlText->c_str());
                     CPLAddXMLChild( pParent->psParent, psNewNode );
                 }
                 pChild = pChild->pNext;

commit 24624106dae8e85f27d9a80f881b9f6a2edc0c36

HugeFileResolver: fix regression caused by r23552 (#4380)
git-svn-id: https://svn.osgeo.org/gdal@23562 f0d54148-0727-0410-94bb-9a71ac55c965

trunk/gdal/ogr/ogrsf_frmts/gml/hugefileresolver.cpp
@@ -1756,7 +1756,8 @@ static int gmlHugeResolveEdges( struct huge_helper *helper,
                         CPLXMLNode *psOrientationNode = CPLCreateXMLNode(psNewNode, CXT_Attribute, "orientation");
                         CPLCreateXMLNode(psOrientationNode, CXT_Text, "-");
                     }
-                    CPLCreateXMLNode(psNewNode, CXT_Text, pChild->pItem->gmlText->c_str());
+                    CPLXMLNode *psEdge = CPLParseXMLString(pChild->pItem->gmlText->c_str());
+                    CPLAddXMLChild( psNewNode, psEdge );
                     CPLAddXMLChild( pParent->psParent, psNewNode );
                 }
                 pChild = pChild->pNext;