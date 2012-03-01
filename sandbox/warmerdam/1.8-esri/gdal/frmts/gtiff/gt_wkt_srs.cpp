77f2d350504f3488942e8a42907d14e5963033c8
rprinceley <rprinceley@f0d54148-0727-0410-94bb-9a71ac55c965>
2012-03-01 08:51:35 +0000

    Revert change to "broken" handling.
    git-svn-id: https://svn.osgeo.org/gdal@24043 f0d54148-0727-0410-94bb-9a71ac55c965

--- sandbox/warmerdam/1.8-esri/gdal/frmts/gtiff/gt_wkt_srs.cpp
+++ sandbox/warmerdam/1.8-esri/gdal/frmts/gtiff/gt_wkt_srs.cpp
@@ -455,7 +455,6 @@ char *GTIFGetOGISDefn( GTIF *hGTIF, GTIFDefn * psDefn )
     GTIFKeyGet(hGTIF, (geokey_t) ProjLinearUnitsInterpCorrectGeoKey, 
                &bLinearUnitsMarkedCorrect, 0, 1);
 
-    bool correctedBROKEN = false;
     if( EQUAL(pszLinearUnits,"BROKEN") 
         && psDefn->Projection == KvUserDefined 
         && !bLinearUnitsMarkedCorrect )
@@ -474,7 +473,6 @@ char *GTIFGetOGISDefn( GTIF *hGTIF, GTIFDefn * psDefn )
                     && psDefn->UOMLengthInMeters != 1.0 )
                 {
                     psDefn->ProjParm[iParm] /= psDefn->UOMLengthInMeters;
-                    correctedBROKEN = true;
                     CPLDebug( "GTIFF", "converting geokey to accomodate old broken file due to GTIFF_LINEAR_UNITS=BROKEN setting." );
                 }
                 break;
@@ -690,7 +688,7 @@ char *GTIFGetOGISDefn( GTIF *hGTIF, GTIFDefn * psDefn )
         }
         int unitCode = 0;
         GTIFKeyGet(hGTIF, ProjLinearUnitsGeoKey, &unitCode, 0, 1  );
-        if(unitCode != KvUserDefined && !correctedBROKEN)
+        if(unitCode != KvUserDefined)
         {
           adfParm[5] /= psDefn->UOMLengthInMeters;
           adfParm[6] /= psDefn->UOMLengthInMeters;
