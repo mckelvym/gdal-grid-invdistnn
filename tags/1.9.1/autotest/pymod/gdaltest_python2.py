19f0e3d8ec85c6a2e06467552784ed88568cfb0c
warmerdam <warmerdam@f0d54148-0727-0410-94bb-9a71ac55c965>
2012-05-17 03:29:34 +0000

    Prepare for 1.9.1RC2
    git-svn-id: https://svn.osgeo.org/gdal@24428 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ tags/1.9.1/autotest/pymod/gdaltest_python2.py
@@ -0,0 +1,160 @@
+###############################################################################
+# $Id$
+# 
+# Project:  GDAL/OGR Test Suite
+# Purpose:  Python Library supporting GDAL/OGR Test Suite
+# Author:   Even Rouault, <even dot rouault at mines dash paris dot org>
+# 
+###############################################################################
+# Copyright (c) 2003, Frank Warmerdam <warmerdam@pobox.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+###############################################################################
+
+import urllib2
+import socket
+import os
+from sys import version_info
+
+def run_func(func):
+    try:
+        result = func()
+        print(result)
+        return result
+    except SystemExit, x:
+        import traceback
+        traceback.print_exc()
+        
+        raise x
+    except:
+        result = 'fail (blowup)'
+        print(result)
+        
+        import traceback
+        traceback.print_exc()    
+        return result
+
+def urlescape(url):
+    # Escape any non-ASCII characters
+    try:
+        import urllib
+        url = urllib.quote(url)
+    except:
+        pass
+    return url
+
+def gdalurlopen(url):
+    timeout = 10
+    old_timeout = socket.getdefaulttimeout()
+    socket.setdefaulttimeout(timeout)
+
+    if 'GDAL_HTTP_PROXY' in os.environ:
+        proxy = os.environ['GDAL_HTTP_PROXY']
+
+        if 'GDAL_HTTP_PROXYUSERPWD' in os.environ:
+            proxyuserpwd = os.environ['GDAL_HTTP_PROXYUSERPWD']
+            proxyHandler = urllib2.ProxyHandler({"http" : \
+                "http://%s@%s" % (proxyuserpwd, proxy)})
+        else:
+            proxyuserpwd = None
+            proxyHandler = urllib2.ProxyHandler({"http" : \
+                "http://%s" % (proxy)})
+
+        opener = urllib2.build_opener(proxyHandler, urllib2.HTTPHandler)
+
+        urllib2.install_opener(opener)
+
+    try:
+        handle = urllib2.urlopen(url)
+        socket.setdefaulttimeout(old_timeout)
+        return handle
+    except urllib2.HTTPError, e:
+        print('HTTP service for %s is down (HTTP Error: %d)' % (url, e.code))
+        socket.setdefaulttimeout(old_timeout)
+        return None
+    except urllib2.URLError, e:
+        print('HTTP service for %s is down (HTTP Error: %s)' % (url, e.reason))
+        socket.setdefaulttimeout(old_timeout)
+        return None
+    except:
+        print('HTTP service for %s is down.' %(url))
+        socket.setdefaulttimeout(old_timeout)
+        return None
+
+def warn_if_memleak(cmd, out_str):
+
+    # If DEBUG_VSIMALLOC_STATS is defined, this is an easy way
+    # to catch some memory leaks
+    if cmd.find('--utility_version') == -1 and \
+       out_str.find('VSIMalloc + VSICalloc - VSIFree') != -1 and \
+       out_str.find('VSIMalloc + VSICalloc - VSIFree : 0') == -1:
+        print('memory leak detected')
+        print(out_str)
+
+def spawn_async26(cmd):
+    import shlex
+    import subprocess
+    command = shlex.split(cmd)
+    try:
+        process = subprocess.Popen(command, stdout=subprocess.PIPE)
+        return (process, process.stdout)
+    except:
+        return (None, None)
+
+def spawn_async(cmd):
+    if version_info >= (2,6,0):
+        return spawn_async26(cmd)
+
+    import popen2
+    try:
+        process = popen2.Popen3(cmd)
+    except:
+        return (None, None)
+    if process is None:
+        return (None, None)
+    process.tochild.close()
+    return (process, process.fromchild)
+
+def wait_process(process):
+    process.wait()
+
+def runexternal(cmd, strin = None, check_memleak = True):
+    if strin is None:
+        out_str = os.popen(cmd).read()
+    else:
+        (ret_stdin, ret_stdout) = os.popen2(cmd)
+        ret_stdin.write(strin)
+        ret_stdin.close()
+        out_str = ret_stdout.read()
+
+    if check_memleak:
+        warn_if_memleak(cmd, out_str)
+
+    return out_str
+
+def runexternal_out_and_err(cmd, check_memleak = True):
+    (ret_stdin, ret_stdout, ret_stderr) = os.popen3(cmd)
+    ret_stdin.close()
+    out_str = ret_stdout.read()
+    err_str = ret_stderr.read()
+
+    if check_memleak:
+        warn_if_memleak(cmd, out_str)
+
+    return (out_str, err_str)
